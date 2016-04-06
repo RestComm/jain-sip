5fdb505cd0527ed7b882bb6b5025c7012467954a
Jean Deruelle <jean.deruelle@telestax.com>
2016-04-06 18:09:01 +0200

    Adding message logging for outbound message in WSS transport
    (cherry picked from commit 132fecd27d729bf3cc2a5f8064dbc9463e7156ae)

--- src/gov/nist/javax/sip/stack/NioTlsWebSocketMessageChannel.java
+++ src/gov/nist/javax/sip/stack/NioTlsWebSocketMessageChannel.java
@@ -28,6 +28,7 @@ package gov.nist.javax.sip.stack;
 import gov.nist.core.CommonLogger;
 import gov.nist.core.LogLevels;
 import gov.nist.core.LogWriter;
+import gov.nist.core.ServerLogger;
 import gov.nist.core.StackLogger;
 import gov.nist.javax.sip.SipStackImpl;
 import gov.nist.javax.sip.message.SIPMessage;
@@ -259,6 +260,9 @@ public class NioTlsWebSocketMessageChannel extends NioWebSocketMessageChannel im
 		// https://java.net/jira/browse/JSIP-497 fix transport for WSS
 		final byte[] msg = sipMessage.encodeAsBytes(this.getTransport());
 		sendMessage(msg, receiverAddress, receiverPort, this.client);
+		
+		if (logger.isLoggingEnabled(ServerLogger.TRACE_MESSAGES))
+			logMessage(sipMessage, receiverAddress, receiverPort, System.currentTimeMillis());
     }
 
 	public void sendHttpMessage(final byte message[], final InetAddress receiverAddress,
