a7bc024b1b27c0c07f896ad356d1ba1ad44cf430
jaime.casero@gmail.com <jaime.casero@gmail.com>
2016-05-11 18:08:37 +0200

    fixes https://github.com/RestComm/jain-sip/issues/92

--- src/gov/nist/javax/sip/parser/StringMsgParserFactory.java
+++ src/gov/nist/javax/sip/parser/StringMsgParserFactory.java
@@ -33,12 +33,17 @@ import gov.nist.javax.sip.stack.SIPTransactionStack;
  */
 public class StringMsgParserFactory implements MessageParserFactory {
 
+    /**
+     * msg parser is completely stateless, reuse isntance for the whole stack
+     * fixes https://github.com/RestComm/jain-sip/issues/92
+     */
+    private static StringMsgParser msgParser = new StringMsgParser();
 	/*
 	 * (non-Javadoc)
 	 * @see gov.nist.javax.sip.parser.MessageParserFactory#createMessageParser(gov.nist.javax.sip.stack.SIPTransactionStack)
 	 */
 	public MessageParser createMessageParser(SIPTransactionStack stack) {	
-		return new StringMsgParser();
+		return msgParser;
 	}
 
 }
