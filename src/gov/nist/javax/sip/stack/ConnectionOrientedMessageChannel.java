475337c9c82d12766ad3f411ad421a7120e7145e
Jean Deruelle <jean.deruelle@telestax.com>
2016-10-31 11:06:17 +0100

    More fixes for https://github.com/RestComm/jain-sip/issues/116
    (cherry picked from commit 41f00c45a2362f1d784c646007b0c257456269ce)

--- src/gov/nist/javax/sip/stack/ConnectionOrientedMessageChannel.java
+++ src/gov/nist/javax/sip/stack/ConnectionOrientedMessageChannel.java
@@ -622,7 +622,7 @@ public abstract class ConnectionOrientedMessageChannel extends MessageChannel im
                     try {
                         if (logger.isLoggingEnabled(LogWriter.TRACE_DEBUG))
                             logger.logDebug(
-                                    "IOException closing sock " + ex);
+                                    "IO issue while closing socket " + ex.getMessage());
                         try {
                             if (sipStack.maxConnections != -1) {
                                 synchronized (messageProcessor) {
